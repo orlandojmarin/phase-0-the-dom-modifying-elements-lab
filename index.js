599938212db15725da32208052d4341dcde4cff9
Orlando Marin <orlandojermainemarin@gmail.com>
2021-02-17 12:07:21 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const main = document.getElementsByTagName('main')[0];
+main.remove();
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.textContent = "Orlando is the champion"
+
+
+
+/*main.removeChild(ul.querySelector('li:nth-child(2)'));
+body.remove(main#main)*/
